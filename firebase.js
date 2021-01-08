3cd67f7fa8e7f55b933fd54e315a2f37167b1ef6
SHIFTx Official <shiftxytofficial@gmail.com>
2021-01-08 12:43:11 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0Be4dgbKnWGMJOHl0BUE8n03Og_BXb-0",
+  authDomain: "main-0211-doc-com.firebaseapp.com",
+  projectId: "main-0211-doc-com",
+  storageBucket: "main-0211-doc-com.appspot.com",
+  messagingSenderId: "70578315803",
+  appId: "1:70578315803:web:9d2e51bb33fbc432110ebe",
+  measurementId: "G-HCQXZTSGXZ"
+};
